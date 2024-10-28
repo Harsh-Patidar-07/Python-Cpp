31bc3164847371d41b36884833b618033ef08972
Harsh-Patidar-07 <harshpatidar16122008@gmail.com>
2024-10-29 01:32:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-from time import sleep
 
-def readFile (file) : # note : The File Path should be either Relatvie or from Root
-    with open(file, "r") as file :
-        first_line = file.readline().strip()
-
-        return first_line
-
-while True :
-    print(readFile('TestFile.txt'), end = '\n\r')
-    sleep(0.5)
